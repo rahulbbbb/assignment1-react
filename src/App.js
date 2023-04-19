849c0f42bd7f997d49c29769c370d489d4179f99
Rahulbbbb <beingbankoti@gmail.com>
2023-04-19 23:09:02 +0530

    solution1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function ReactLogo(){
+  return(
+    <>
+    <img src={logo} className="App-logo" alt="logo" />
+    </>
+  )
+}
+
+ function ReactBenefits(){
+  return(
+    <>
+    <h1>Benefits of using react</h1>
+    <li>Component-based architecture</li>
+      <li>Virtual DOM for efficient updates</li>
+    <li>Rich ecosystem and community</li>
+    <li>Cross-platform development</li>
+    <li>Strong community support</li>
+    
+    </>
+  )
+}
+
+function Button(){
+  return(
+    <button>Get Started...</button>
+  )
+}
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+        <ReactLogo/>
+        <ReactBenefits/>
+        <Button/>
+      
     </div>
   );
 }
